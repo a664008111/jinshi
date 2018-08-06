91c6af73fc62c138b9f8e64c2dbed7227484f004
a664008111 <834321724@qq.com>
2018-08-06 11:09:23 +0800

    晋石

---
+++ advance/src/components/AboutUs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="AboutUs">
+      <div class="abou">
+        <h2>
+          <img :src="aboutus.longImg" alt="">
+            <li>
+                <span><img :src="aboutus.absuImg" alt=""><b>{{aboutus.mi}}</b></span>
+                <b>{{aboutus.chuang}}</b>
+            </li>
+        </h2>
+        <div class="absou-list">
+            <h1><i class="el-icon-caret-right"></i> {{aboutus.AboutUsli}}</h1>
+            <p><span><img :src="aboutus.abouImg" alt=""></span> <b>{{aboutus.textP}}</b></p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+        aboutus:[]
+      }
+    },
+    methods:{
+      init(){
+        fetch("https://www.easy-mock.com/mock/5b480d3dae45641f63210b3b/example/advance#!method=get")
+        .then(res=>{
+          res.json().then(body=>{
+            this.aboutus=body.data.AboutUs
+          })
+        })
+      }
+    },
+    mounted(){
+      this.init()
+    }
+}
+</script>
+
+<style>
+@import "../common/style/AboutUs.css";
+</style>
